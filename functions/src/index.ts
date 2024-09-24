dc417111457176b781cd27332d22265107c67d56
derickben <benedictuyioghosas@gmail.com>
2024-09-23 21:33:28 -0400

    store secrets securely

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
-import admin from 'firebase-admin'
-import serviceAccount from './utils/serviceAccountKey.json'
+import admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+import serviceAccount from './utils/serviceAccountKey.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as never),
   databaseURL: 'https://risefunds-default-rtdb.firebaseio.com/',
-})
+});
 
-export * from './api'
+export * from './api';
 // export * from './events'
 // export * from './cron'
