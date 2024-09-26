2f81266dc039e046c9eb4fd3364e67b4e7bf5dc8
derickben <benedictuyioghosas@gmail.com>
2024-09-26 00:51:57 -0400

    update 2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,16 @@
 import admin from 'firebase-admin';
-// import serviceAccount from './utils/serviceAccountKey.json';
+import serviceAccount from './utils/serviceAccountKey.json';
 
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const serviceAccount = JSON.parse(
-  process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64 as string
-);
+// const serviceAccount = JSON.parse(
+//   process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64 as string
+// );
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as never),
   databaseURL: 'https://risefunds-default-rtdb.firebaseio.com/',
 });
 
