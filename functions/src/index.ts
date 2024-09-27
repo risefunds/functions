e831b1593de1e54306d7b2cfc5ac73db3c0fa39e
derickben <benedictuyioghosas@gmail.com>
2024-09-27 05:16:50 -0400

    base64

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,23 @@
 import admin from 'firebase-admin';
-import serviceAccount from './utils/serviceAccountKey.json';
+// import serviceAccount from './utils/serviceAccountKey.json';
 
-// import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// const serviceAccount = JSON.parse(
-//   process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64 as string
-// );
+let serviceAccount: admin.ServiceAccount;
+
+if (process.env.GITHUB_ACTIONS) {
+  // GitHub Actions: Decode the Base64-encoded service account credentials
+  const decodedCredentials = Buffer.from(
+    process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64!,
+    'base64'
+  ).toString('utf-8');
+  serviceAccount = JSON.parse(decodedCredentials);
+} else {
+  // Local environment: Use JSON directly from the .env file
+  serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as never),
